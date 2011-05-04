f9792039ceefd78240f33e9ad293199987e2358b
Elis?e Maurer <emaurer@creative-patterns.com>
2011-05-04 18:09:03 +0200

    Allow updating Label text & update anchor accordingly

--- VectorUI/Widgets/Label.cs
+++ VectorUI/Widgets/Label.cs
@@ -9,22 +9,19 @@ using Microsoft.Xna.Framework;
 
 namespace VectorUI.Widgets
 {
-    class Label: Widget
+    public class Label: Widget
     {
         //----------------------------------------------------------------------
         public Label( UISheet _sheet, Text _text )
         : base( _text.Name, _sheet )
         {
-            Text = UISheet.Game.GetUIString( _text.TextSpans[0].Value.Substring( "String".Length ) );
-
             //Vector2 vSize = UISheet.Font.MeasureString( Text );
             mvPosition = _text.Position - new Vector2( 0, /* FIXME: hard-coded stuff! */ 35 );
             mvOrigin = Vector2.Zero;
 
-            if( _text.Anchor == TextAnchor.Middle )
-            {
-                mvOrigin = new Vector2( UISheet.Font.MeasureString( Text ).X / 2f, 0f );
-            }
+            mAnchor = _text.Anchor;
+            Text = UISheet.Game.GetUIString( _text.TextSpans[0].Value.Substring( "String".Length ) );
+
 
             mColor = _text.FillColor;
         }
@@ -41,7 +38,48 @@ namespace VectorUI.Widgets
         }
 
         //----------------------------------------------------------------------
-        public string   Text;
+        public string   Text
+        {
+            get {
+                return mstrText;
+            }
+
+            set {
+                mstrText = value;
+
+                switch( Anchor )
+                {
+                    case TextAnchor.Start:
+                        mvOrigin = new Vector2( 0f, 0f );
+                        break;
+                    case TextAnchor.Middle:
+                        mvOrigin = new Vector2( UISheet.Font.MeasureString( mstrText ).X / 2f, 0f );
+                        break;
+                    case TextAnchor.End:
+                        mvOrigin = new Vector2( UISheet.Font.MeasureString( mstrText ).X, 0f );
+                        break;
+                }
+            }
+        }
+
+        string          mstrText;
+
+        TextAnchor      Anchor
+        {
+            get {
+                return mAnchor;
+            }
+
+            set {
+                mAnchor = value;
+                if( mAnchor == TextAnchor.Middle )
+                {
+                    mvOrigin = new Vector2( UISheet.Font.MeasureString( Text ).X / 2f, 0f );
+                }
+            }
+        }
+
+        TextAnchor      mAnchor;
 
         Vector2         mvPosition;
         Vector2         mvOrigin;
