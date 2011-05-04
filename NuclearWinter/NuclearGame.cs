b0ca55dca31b5b7ea37ef685b8ee799c142d6b0a
Elis?e Maurer <emaurer@creative-patterns.com>
2011-05-04 10:36:36 +0200

    Add WrapText & DrawBlurredText from QuadSmash to NuclearGame

--- NuclearWinter/NuclearGame.cs
+++ NuclearWinter/NuclearGame.cs
@@ -41,6 +41,106 @@ namespace NuclearWinter
         public virtual string GetUIString( string _strId ) { return _strId; }
 
         //----------------------------------------------------------------------
+        public List<string> WrapText( SpriteFont _font, string _strText, float _fLineWidth )
+        {
+            List<string> lText = new List<string>();
+
+            string strLine = string.Empty;
+            string returnString = String.Empty;
+            string[] aWords = _strText.Split(' ');
+
+            foreach( string strWord in aWords )
+            {
+                if( _font.MeasureString(strLine + strWord).Length() > _fLineWidth )
+                {
+                    lText.Add( strLine );
+                    strLine = string.Empty;
+                }
+
+                strLine += strWord + ' ';
+            }
+
+            lText.Add( strLine );
+
+            return lText;
+        }
+
+        //----------------------------------------------------------------------
+        public void DrawBlurredText( SpriteFont _font, string _strLabel, Vector2 _vPosition )
+        {
+            DrawBlurredText( _font, _strLabel, _vPosition, Color.White, Color.Black, Vector2.Zero, 1f  );
+        }
+
+        //----------------------------------------------------------------------
+        public void DrawBlurredText( SpriteFont _font, string _strLabel, Vector2 _vPosition, Color _color )
+        {
+            DrawBlurredText( _font, _strLabel, _vPosition, _color, Color.Black, Vector2.Zero, 1f );
+        }
+
+        //----------------------------------------------------------------------
+        public void DrawBlurredText( SpriteFont _font, string _strLabel, Vector2 _vPosition, Color _color, Vector2 _vOrigin, float _fScale )
+        {
+            DrawBlurredText( _font, _strLabel, _vPosition, _color, Color.Black, _vOrigin, _fScale );
+        }
+
+        //----------------------------------------------------------------------
+        public void DrawBlurredText( SpriteFont _font, string _strLabel, Vector2 _vPosition, Color _color, Color _blurColor )
+        {
+            DrawBlurredText( _font, _strLabel, _vPosition, _color, _blurColor, Vector2.Zero, 1f );
+        }
+
+        //----------------------------------------------------------------------
+        public void DrawBlurredText( SpriteFont _font, string _strLabel, Vector2 _vPosition, Color _color, Color _blurColor, Vector2 _vOrigin, float _fScale )
+        {
+            Color blurColor = _blurColor * 0.1f * (_color.A / 255f);
+
+            SpriteBatch.DrawString( _font, _strLabel, new Vector2( _vPosition.X - 4, _vPosition.Y - 4 ), blurColor, 0f, _vOrigin, _fScale, SpriteEffects.None, 0f );
+            SpriteBatch.DrawString( _font, _strLabel, new Vector2( _vPosition.X + 4, _vPosition.Y - 4 ), blurColor, 0f, _vOrigin, _fScale, SpriteEffects.None, 0f );
+            SpriteBatch.DrawString( _font, _strLabel, new Vector2( _vPosition.X + 4, _vPosition.Y + 4 ), blurColor, 0f, _vOrigin, _fScale, SpriteEffects.None, 0f );
+            SpriteBatch.DrawString( _font, _strLabel, new Vector2( _vPosition.X - 4, _vPosition.Y + 4 ), blurColor, 0f, _vOrigin, _fScale, SpriteEffects.None, 0f );
+
+            SpriteBatch.DrawString( _font, _strLabel, new Vector2( _vPosition.X, _vPosition.Y ), _color, 0f, _vOrigin, _fScale, SpriteEffects.None, 0f );
+        }
+
+        //----------------------------------------------------------------------
+        public void DrawBlurredText( SpriteFont _font, StringBuilder _strbLabel, Vector2 _vPosition )
+        {
+            DrawBlurredText( _font, _strbLabel, _vPosition, Color.White, Color.Black, Vector2.Zero, 1f  );
+        }
+
+        //----------------------------------------------------------------------
+        public void DrawBlurredText( SpriteFont _font, StringBuilder _strbLabel, Vector2 _vPosition, Color _color )
+        {
+            DrawBlurredText( _font, _strbLabel, _vPosition, _color, Color.Black, Vector2.Zero, 1f );
+        }
+
+        //----------------------------------------------------------------------
+        public void DrawBlurredText( SpriteFont _font, StringBuilder _strbLabel, Vector2 _vPosition, Color _color, Vector2 _vOrigin, float _fScale )
+        {
+            DrawBlurredText( _font, _strbLabel, _vPosition, _color, Color.Black, _vOrigin, _fScale );
+        }
+
+        //----------------------------------------------------------------------
+        public void DrawBlurredText( SpriteFont _font, StringBuilder _strbLabel, Vector2 _vPosition, Color _color, Color _blurColor )
+        {
+            DrawBlurredText( _font, _strbLabel, _vPosition, _color, _blurColor, Vector2.Zero, 1f );
+        }
+
+
+        //----------------------------------------------------------------------
+        public void DrawBlurredText( SpriteFont _font, StringBuilder _strbLabel, Vector2 _vPosition, Color _color, Color _blurColor, Vector2 _vOrigin, float _fScale )
+        {
+            Color blurColor = _blurColor * 0.1f * (_color.A / 255f);
+
+            SpriteBatch.DrawString( _font, _strbLabel, new Vector2( _vPosition.X - 4, _vPosition.Y - 4 ), blurColor, 0f, _vOrigin, _fScale, SpriteEffects.None, 0f );
+            SpriteBatch.DrawString( _font, _strbLabel, new Vector2( _vPosition.X + 4, _vPosition.Y - 4 ), blurColor, 0f, _vOrigin, _fScale, SpriteEffects.None, 0f );
+            SpriteBatch.DrawString( _font, _strbLabel, new Vector2( _vPosition.X + 4, _vPosition.Y + 4 ), blurColor, 0f, _vOrigin, _fScale, SpriteEffects.None, 0f );
+            SpriteBatch.DrawString( _font, _strbLabel, new Vector2( _vPosition.X - 4, _vPosition.Y + 4 ), blurColor, 0f, _vOrigin, _fScale, SpriteEffects.None, 0f );
+
+            SpriteBatch.DrawString( _font, _strbLabel, new Vector2( _vPosition.X, _vPosition.Y ), _color, 0f, _vOrigin, _fScale, SpriteEffects.None, 0f );
+        }
+
+        //----------------------------------------------------------------------
         public GraphicsDeviceManager                        Graphics;
         public SpriteBatch                                  SpriteBatch;
 
