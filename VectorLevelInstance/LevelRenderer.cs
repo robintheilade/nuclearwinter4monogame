9bb4b410f24e8321e75cfd4ff49b3fb7f5ca3a8d
Elis?e Maurer <elisee@sparklin.org>
2011-05-08 16:29:41 +0200

    * Remove useless references & using  * Remove cloned project files for Box2D.XNA  * Add wp7 version of VectorLevelInstance project file

--- VectorLevelInstance/LevelRenderer.cs
+++ VectorLevelInstance/LevelRenderer.cs
@@ -3,14 +3,9 @@ using System.Collections.Generic;
 using System.Diagnostics;
 
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
-using Microsoft.Xna.Framework.Media;
-using Microsoft.Xna.Framework.Net;
-using Microsoft.Xna.Framework.Storage;
 
 namespace VectorLevel
 {
