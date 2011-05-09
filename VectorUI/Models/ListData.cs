f67fedf7f44b6d2dd8fa0b4291cdbd94684b54f7
Elis?e Maurer <emaurer@creative-patterns.com>
2011-05-09 13:54:49 +0200

    Add ability to add a Tag to a ListData entry

--- VectorUI/Models/ListData.cs
+++ VectorUI/Models/ListData.cs
@@ -6,44 +6,55 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace VectorUI.Models
 {
+    //--------------------------------------------------------------------------
     public enum ListColumnType
     {
         Text,
         Image
     }
 
+    //--------------------------------------------------------------------------
     public class ListColumn
     {
+        //----------------------------------------------------------------------
         public ListColumn( ListColumnType _columnType, int _size )
         {
             ColumnType  = _columnType;
             Size        = _size;
         }
 
+        //----------------------------------------------------------------------
         public ListColumnType       ColumnType;
         public int                  Size;
     }
 
+    //--------------------------------------------------------------------------
     public class ListEntry
     {
         public string[]             Values;
+        public object               Tag;
     }
 
+    //--------------------------------------------------------------------------
     public class ListData
     {
+        //----------------------------------------------------------------------
         public ListData()
         {
             Entries = new List<ListEntry>();
         }
 
-        public void AddEntry( string[] _values )
+        //----------------------------------------------------------------------
+        public void AddEntry( string[] _values, object _tag )
         {
             var entry = new ListEntry();
             entry.Values = _values;
+            entry.Tag = _tag;
 
             Entries.Add( entry );
         }
 
+        //----------------------------------------------------------------------
         public ListColumn[]         Columns;
         public List<ListEntry>      Entries;
     }
