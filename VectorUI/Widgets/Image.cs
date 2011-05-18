647a0cb2b78ae809b0425541d094b27fecc57a8a
Elis?e Maurer <emaurer@creative-patterns.com>
2011-05-18 15:25:08 +0200

    VectorUI: Allow changing image texture

--- VectorUI/Widgets/Image.cs
+++ VectorUI/Widgets/Image.cs
@@ -15,7 +15,7 @@ namespace VectorUI.Widgets
         public Image( UISheet _sheet, Marker _marker )
         : base( _marker.Name, _sheet )
         {
-            mTexture = UISheet.Game.Content.Load<Texture2D>( _marker.MarkerFullPath );
+            Texture = UISheet.Game.Content.Load<Texture2D>( _marker.MarkerFullPath );
 
             Matrix matrix = Matrix.CreateScale( _marker.Scale.X, _marker.Scale.Y, 0f ) * Matrix.CreateRotationZ( _marker.Angle );
             mfAngle = (float)Math.Atan2( matrix.M12 /* cos angle */, matrix.M11 /* sin angle */ );
@@ -23,8 +23,8 @@ namespace VectorUI.Widgets
             Vector2 vRotatedCenter = Vector2.Transform( _marker.Size / 2f, matrix );
             mvPosition = _marker.Position + vRotatedCenter;
 
-            mvOrigin = new Vector2( mTexture.Width, mTexture.Height ) / 2f;
-            mvScale = _marker.Size / new Vector2( mTexture.Width, mTexture.Height ) * _marker.Scale;
+            mvOrigin = new Vector2( Texture.Width, Texture.Height ) / 2f;
+            mvScale = _marker.Size / new Vector2( Texture.Width, Texture.Height ) * _marker.Scale;
 
             mColor = _marker.Color;
         }
@@ -37,16 +37,16 @@ namespace VectorUI.Widgets
         //----------------------------------------------------------------------
         public override void Draw()
         {
-            UISheet.Game.SpriteBatch.Draw( mTexture, mvPosition + Offset, null, mColor * Opacity, mfAngle, mvOrigin, mvScale * Scale, SpriteEffects.None, 0f );
+            UISheet.Game.SpriteBatch.Draw( Texture, mvPosition + Offset, null, mColor * Opacity, mfAngle, mvOrigin, mvScale * Scale, SpriteEffects.None, 0f );
         }
 
         //----------------------------------------------------------------------
-        Texture2D       mTexture;
+        public Texture2D    Texture;
 
-        Vector2         mvPosition;
-        float           mfAngle;
-        Vector2         mvOrigin;
-        Vector2         mvScale;
-        Color           mColor;
+        Vector2             mvPosition;
+        float               mfAngle;
+        Vector2             mvOrigin;
+        Vector2             mvScale;
+        Color               mColor;
     }
 }
