a4a732892aafba34ab3c6c77f1de6972f25a4125
Elis?e Maurer <elisee@sparklin.org>
2011-09-12 19:28:31 +0200

    Allow resizing PopupPane and put panel & page container in it

--- NuclearWinter/UI/Menu/MessagePopupPane.cs
+++ NuclearWinter/UI/Menu/MessagePopupPane.cs
@@ -7,17 +7,12 @@ namespace NuclearWinter.UI
 {
     public class MessagePopupPane: PopupPane<IMenuManager>
     {
-        public const int        Width   = 800;
-        public const int        Height  = 450;
+        public Label                TitleLabel      { get; private set; }
+        public Label                MessageLabel    { get; private set; }
+        public Button               CloseButton     { get; private set; }
 
-        public string           Hostname;
-
-        public NuclearWinter.UI.Label         TitleLabel      { get; private set; }
-        public NuclearWinter.UI.Label         MessageLabel    { get; private set; }
-        public NuclearWinter.UI.Button        CloseButton     { get; private set; }
-
-        NuclearWinter.UI.FixedWidget          mSpinningWheelAnchor;
-        NuclearWinter.UI.SpinningWheel        mSpinningWheel;
+        FixedWidget                 mSpinningWheelAnchor;
+        SpinningWheel               mSpinningWheel;
 
         public bool ShowSpinningWheel {
             set
@@ -37,37 +32,33 @@ namespace NuclearWinter.UI
         public MessagePopupPane( IMenuManager _manager )
         : base( _manager )
         {
-            FixedGroup = new NuclearWinter.UI.FixedGroup( Manager.PopupScreen );
-
             int iPadding = FixedGroup.Screen.Style.PopupFrameCornerSize;
 
-            NuclearWinter.UI.Panel panel = new NuclearWinter.UI.Panel( FixedGroup.Screen, FixedGroup.Screen.Style.PopupFrame, FixedGroup.Screen.Style.PopupFrameCornerSize );
-            FixedGroup.AddChild( new NuclearWinter.UI.FixedWidget( panel, AnchoredRect.CreateCentered( Width, Height ) ) );
+            TitleLabel = new Label( Manager.MenuScreen, "", Anchor.Start );
+            TitleLabel.Font = Manager.MenuScreen.Style.BigFont;
 
             FixedGroup page = new FixedGroup( FixedGroup.Screen );
-            FixedGroup.AddChild( new FixedWidget( page, AnchoredRect.CreateCentered( Width, Height ) ) );
+            mPageContainer.Child = page;
 
-            TitleLabel = new NuclearWinter.UI.Label( Manager.MenuScreen, "", NuclearWinter.UI.Anchor.Start );
-            TitleLabel.Font = Manager.MenuScreen.Style.BigFont;
             page.AddChild( new FixedWidget( TitleLabel, AnchoredRect.CreateTopAnchored( iPadding, iPadding, iPadding, FixedGroup.Screen.Style.DefaultButtonHeight ) ) );
 
             {
-                mSpinningWheel = new NuclearWinter.UI.SpinningWheel( FixedGroup.Screen, FixedGroup.Screen.Style.SpinningWheel );
-                mSpinningWheelAnchor = new NuclearWinter.UI.FixedWidget( FixedGroup.Screen, AnchoredRect.CreateCentered( mSpinningWheel.ContentWidth , mSpinningWheel.ContentHeight ) );
+                mSpinningWheel = new SpinningWheel( FixedGroup.Screen, FixedGroup.Screen.Style.SpinningWheel );
+                mSpinningWheelAnchor = new FixedWidget( FixedGroup.Screen, AnchoredRect.CreateCentered( mSpinningWheel.ContentWidth, mSpinningWheel.ContentHeight ) );
                 page.AddChild( mSpinningWheelAnchor );
 
                 // Message label
-                MessageLabel = new NuclearWinter.UI.Label( FixedGroup.Screen, "", NuclearWinter.UI.Anchor.Start );
+                MessageLabel = new Label( FixedGroup.Screen, "", Anchor.Start );
                 MessageLabel.WrapText = true;
-                page.AddChild( new NuclearWinter.UI.FixedWidget( MessageLabel, AnchoredRect.CreateTopAnchored( iPadding, iPadding + 80, iPadding, 200 ) ) );
+                page.AddChild( new FixedWidget( MessageLabel, AnchoredRect.CreateTopAnchored( iPadding, iPadding + 80, iPadding, 200 ) ) );
 
                 // Actions
-                NuclearWinter.UI.BoxGroup actionsGroup = new NuclearWinter.UI.BoxGroup( FixedGroup.Screen, NuclearWinter.UI.Orientation.Horizontal, 0 );
+                BoxGroup actionsGroup = new BoxGroup( FixedGroup.Screen, Orientation.Horizontal, 0 );
                 page.AddChild( new FixedWidget( actionsGroup, AnchoredRect.CreateBottomAnchored( iPadding, iPadding, iPadding, FixedGroup.Screen.Style.DefaultButtonHeight ), Anchor.End ) );
 
                 // Close
                 // FIXME: i18n
-                CloseButton = new NuclearWinter.UI.Button( FixedGroup.Screen, "Close" );
+                CloseButton = new Button( FixedGroup.Screen, "Close" );
                 CloseButton.BindPadButton( Buttons.A );
                 actionsGroup.AddChild( CloseButton );
             }
--- NuclearWinter/UI/Menu/PopupPane.cs
+++ NuclearWinter/UI/Menu/PopupPane.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
 
 namespace NuclearWinter.UI
 {
@@ -9,11 +10,42 @@ namespace NuclearWinter.UI
     {
         public T                    Manager { get; private set; }
 
+        public Point Size {
+            get { return mSize; }
+            set {
+                mSize = value;
+                mPanelContainer.ChildBox.Width = mSize.X;
+                mPanelContainer.ChildBox.Height = mSize.Y;
+
+                mPageContainer.ChildBox.Width = mSize.X;
+                mPageContainer.ChildBox.Height = mSize.Y;
+            }
+        }
+
+        Point                       mSize               = new Point( 800, 450 );
+        FixedWidget                 mPanelContainer;
+        protected FixedWidget       mPageContainer;
+
         //----------------------------------------------------------------------
         public PopupPane( T _manager )
         {
             Manager     = _manager;
             FixedGroup  = new NuclearWinter.UI.FixedGroup( Manager.PopupScreen );
+
+            Panel panel = new Panel( FixedGroup.Screen, FixedGroup.Screen.Style.PopupFrame, FixedGroup.Screen.Style.PopupFrameCornerSize );
+
+            mPanelContainer = new FixedWidget( panel, AnchoredRect.CreateCentered( mSize.X, mSize.Y ) );
+            FixedGroup.AddChild( mPanelContainer );
+
+            mPageContainer = new FixedWidget( FixedGroup.Screen, AnchoredRect.CreateCentered( mSize.X, mSize.Y ) );
+            FixedGroup.AddChild( mPageContainer );
+        }
+
+        //----------------------------------------------------------------------
+        public void Open( int _iWidth, int _iHeight )
+        {
+            Size = new Point( _iWidth, _iHeight );
+            Open();
         }
     }
 }
