368a1301c81627208adb7822bb5fbfebabd3db9f
Elis?e Maurer <elisee@sparklin.org>
2011-10-15 17:58:34 +0200

    Rename CustomViewportHandler to ICustomViewportHandler

--- NuclearWinter/UI/CustomViewport.cs
+++ NuclearWinter/UI/CustomViewport.cs
@@ -7,7 +7,7 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace NuclearWinter.UI
 {
-    public interface CustomViewportHandler
+    public interface ICustomViewportHandler
     {
         void OnMouseEnter( Point _hitPoint );
         void OnMouseMove( Point _hitPoint );
@@ -23,7 +23,7 @@ namespace NuclearWinter.UI
     {
         public Action<float>                UpdateHandler;
         public Action                       DrawHandler;
-        public CustomViewportHandler        EventHandler;
+        public ICustomViewportHandler       EventHandler;
 
         //----------------------------------------------------------------------
         public CustomViewport( Screen _screen )
