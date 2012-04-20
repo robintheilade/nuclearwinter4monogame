6eadc7b9348608bbe7b97cc69434331556f2cca1
Elis?e Maurer <elisee@sparklin.org>
2012-04-20 23:21:13 +0200

    Add ProgressBar stub

---
+++ NuclearWinter/UI/ProgressBar.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NuclearWinter.UI
+{
+    //--------------------------------------------------------------------------
+    public class ProgressBar: Widget
+    {
+        //----------------------------------------------------------------------
+
+        //----------------------------------------------------------------------
+        public ProgressBar( Screen _screen )
+        : base( _screen )
+        {
+
+        }
+
+        //----------------------------------------------------------------------
+        internal override void Update( float _fElapsedTime )
+        {
+        }
+
+        //----------------------------------------------------------------------
+        internal override void Draw()
+        {
+            
+        }
+    }
+}
