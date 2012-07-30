08121814908451e9d006045c8a87316b0acfb2f9
Elis?e Maurer <elisee@sparklin.org>
2012-07-30 18:01:06 +0200

    Add ApplicationMutex, can be used to prevent running twice the same game

--- NuclearSample/NuclearSample/Program.cs
+++ NuclearSample/NuclearSample/Program.cs
@@ -2,20 +2,27 @@ using System;
 
 namespace NuclearSample
 {
-#if WINDOWS || XBOX
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        static void Main(string[] args)
+        static void Main( string[] args )
         {
-            using (NuclearSampleGame game = new NuclearSampleGame())
+            // This sample uses an ApplicationMutex to prevent running the game multiple times at once.
+            // Useful for games where progress or settings might be overwritten if two instances are running at the same time.
+            using( NuclearWinter.ApplicationMutex mutex = new NuclearWinter.ApplicationMutex() )
             {
-                game.Run();
+                if( mutex.HasHandle )
+                {
+                    using( NuclearSampleGame game = new NuclearSampleGame() )
+                    {
+                        game.Run();
+                    }
+                }
+                else
+                {
+                    System.Windows.Forms.MessageBox.Show( "An instance of NuclearSample is already running.", "Could not start NuclearSample", System.Windows.Forms.MessageBoxButtons.OK, System.Windows.Forms.MessageBoxIcon.Stop );
+                }
             }
         }
     }
-#endif
 }
 
