d557b4d9426cc430450b806fec49f4443d1eceb2
Robin Theilade <robintheilade@users.noreply.github.com>
2016-07-06 17:43:53 +0200

    Remove unused namespace usings Rename method parameters to get closer to Microsoft naming guidelines

--- NuclearWinter/UI/Menu/IMenuManager.cs
+++ NuclearWinter/UI/Menu/IMenuManager.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Microsoft.Xna.Framework.Content;
+﻿using Microsoft.Xna.Framework.Content;
 
 namespace NuclearWinter.UI
 {
@@ -10,13 +6,13 @@ namespace NuclearWinter.UI
     public interface IMenuManager
     {
         //----------------------------------------------------------------------
-        ContentManager      Content         { get; }
+        ContentManager Content { get; }
 
         //----------------------------------------------------------------------
-        Screen              MenuScreen      { get; }
-        Screen              PopupScreen     { get; }
+        Screen MenuScreen { get; }
+        Screen PopupScreen { get; }
 
-        void PushPopup( Panel _popup );
-        void PopPopup( Panel _popup );
+        void PushPopup(Panel popup);
+        void PopPopup(Panel popup);
     }
 }
