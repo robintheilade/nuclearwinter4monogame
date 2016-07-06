aed0c487da3d8e0584cfd5f3b088e530c30152a3
Robin Theilade <robintheilade@users.noreply.github.com>
2016-07-06 16:18:29 +0200

    Remove unused namespace usings Rename method parameters to get closer to Microsoft naming guidelines

--- NuclearWinter/UI/Button.cs
+++ NuclearWinter/UI/Button.cs
@@ -1,99 +1,98 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework;
 using NuclearWinter.Animation;
-using Microsoft.Xna.Framework.Input;
+using System;
 
 namespace NuclearWinter.UI
 {
     /*
      * A clickable Button containing a Label and an optional Image icon
      */
-    public class Button: Widget
+    public class Button : Widget
     {
         //----------------------------------------------------------------------
         public struct ButtonStyle
         {
             //------------------------------------------------------------------
-            public int              CornerSize;
-            public Texture2D        Frame;
-            public Texture2D        DownFrame;
-            public Texture2D        HoverOverlay;
-            public Texture2D        DownOverlay;
-            public Texture2D        FocusOverlay;
-            public int              VerticalPadding;
-            public int              HorizontalPadding;
+            public int CornerSize;
+            public Texture2D Frame;
+            public Texture2D DownFrame;
+            public Texture2D HoverOverlay;
+            public Texture2D DownOverlay;
+            public Texture2D FocusOverlay;
+            public int VerticalPadding;
+            public int HorizontalPadding;
 
             //------------------------------------------------------------------
             public ButtonStyle(
-                int         _iCornerSize,
-                Texture2D   _buttonFrame,
-                Texture2D   _buttonFrameDown,
-                Texture2D   _buttonFrameHover,
-                Texture2D   _buttonFramePressed,
-                Texture2D   _buttonFrameFocused,
-                int         _iVerticalPadding,
-                int         _iHorizontalPadding
+                int _iCornerSize,
+                Texture2D _buttonFrame,
+                Texture2D _buttonFrameDown,
+                Texture2D _buttonFrameHover,
+                Texture2D _buttonFramePressed,
+                Texture2D _buttonFrameFocused,
+                int _iVerticalPadding,
+                int _iHorizontalPadding
             )
             {
-                CornerSize      = _iCornerSize;
-                Frame           = _buttonFrame;
-                DownFrame       = _buttonFrameDown;
-                HoverOverlay    = _buttonFrameHover;
-                DownOverlay     = _buttonFramePressed;
-                FocusOverlay    = _buttonFrameFocused;
+                CornerSize = _iCornerSize;
+                Frame = _buttonFrame;
+                DownFrame = _buttonFrameDown;
+                HoverOverlay = _buttonFrameHover;
+                DownOverlay = _buttonFramePressed;
+                FocusOverlay = _buttonFrameFocused;
 
-                VerticalPadding     = _iVerticalPadding;
-                HorizontalPadding   = _iHorizontalPadding;
+                VerticalPadding = _iVerticalPadding;
+                HorizontalPadding = _iHorizontalPadding;
             }
         }
 
-        public UIFont Font {
+        public UIFont Font
+        {
             get { return mLabel.Font; }
             set { mLabel.Font = value; UpdateContentSize(); }
         }
 
         //----------------------------------------------------------------------
-        Label                   mLabel;
-        Image                   mIcon;
+        Label mLabel;
+        Image mIcon;
 
-        bool                    mbIsHovered;
+        bool mbIsHovered;
 
-        AnimatedValue           mPressedAnim;
-        bool                    mbIsPressed;
+        AnimatedValue mPressedAnim;
+        bool mbIsPressed;
 
-        protected Box           mMargin;
-        public Box              Margin
+        protected Box mMargin;
+        public Box Margin
         {
             get { return mMargin; }
 
-            set {
+            set
+            {
                 mMargin = value;
                 UpdateContentSize();
             }
         }
 
-        public string           Text
+        public string Text
         {
             get
             {
                 return mLabel.Text;
             }
-            
+
             set
             {
                 mLabel.Text = value;
-                mLabel.Padding = mLabel.Text != "" ? new Box( Style.VerticalPadding, Style.HorizontalPadding ) : new Box( Style.VerticalPadding, Style.HorizontalPadding, Style.VerticalPadding, 0 );
+                mLabel.Padding = mLabel.Text != "" ? new Box(Style.VerticalPadding, Style.HorizontalPadding) : new Box(Style.VerticalPadding, Style.HorizontalPadding, Style.VerticalPadding, 0);
                 UpdateContentSize();
             }
         }
 
-        public Texture2D        Icon
+        public Texture2D Icon
         {
-            get {
+            get
+            {
                 return mIcon.Texture;
             }
 
@@ -104,9 +103,10 @@ namespace NuclearWinter.UI
             }
         }
 
-        public Color            IconColor
+        public Color IconColor
         {
-            get {
+            get
+            {
                 return mIcon.Color;
             }
 
@@ -120,7 +120,8 @@ namespace NuclearWinter.UI
         Anchor mAnchor;
         public Anchor Anchor
         {
-            get {
+            get
+            {
                 return mAnchor;
             }
 
@@ -138,163 +139,164 @@ namespace NuclearWinter.UI
 
         public ButtonStyle Style;
 
-        public Action<Button>   ClickHandler;
-        public object           Tag;
+        public Action<Button> ClickHandler;
+        public object Tag;
 
-        public string       TooltipText
+        public string TooltipText
         {
             get { return mTooltip.Text; }
             set { mTooltip.Text = value; }
         }
 
         //----------------------------------------------------------------------
-        Tooltip             mTooltip;
+        Tooltip mTooltip;
 
         //----------------------------------------------------------------------
-        public Button( Screen _screen, ButtonStyle _style, string _strText = "", Texture2D _iconTex = null, Anchor _anchor = Anchor.Center, string _strTooltipText="", object _tag=null )
-        : base( _screen )
+        public Button(Screen screen, ButtonStyle style, string text = "", Texture2D iconTex = null, Anchor anchor = Anchor.Center, string tooltipText = "", object tag = null)
+        : base(screen)
         {
-            Style = _style;
+            Style = style;
 
-            mPadding    = new Box(0);
-            mMargin     = new Box(0);
+            mPadding = new Box(0);
+            mMargin = new Box(0);
 
-            mLabel          = new Label( _screen );
+            mLabel = new Label(screen);
 
-            mIcon           = new Image( _screen );
-            mIcon.Texture   = _iconTex;
-            mIcon.Padding   = new Box( Style.VerticalPadding, 0, Style.VerticalPadding, Style.HorizontalPadding );
+            mIcon = new Image(screen);
+            mIcon.Texture = iconTex;
+            mIcon.Padding = new Box(Style.VerticalPadding, 0, Style.VerticalPadding, Style.HorizontalPadding);
 
-            Text            = _strText;
-            TextColor       = Screen.Style.DefaultTextColor;
+            Text = text;
+            TextColor = Screen.Style.DefaultTextColor;
 
-            Anchor          = _anchor;
+            Anchor = anchor;
 
-            mPressedAnim    = new SmoothValue( 1f, 0f, 0.2f );
-            mPressedAnim.SetTime( mPressedAnim.Duration );
+            mPressedAnim = new SmoothValue(1f, 0f, 0.2f);
+            mPressedAnim.SetTime(mPressedAnim.Duration);
 
-            mTooltip        = new Tooltip( Screen, "" );
+            mTooltip = new Tooltip(Screen, "");
 
-            TooltipText     = _strTooltipText;
-            Tag             = _tag;
+            TooltipText = tooltipText;
+            Tag = tag;
 
             UpdateContentSize();
         }
 
         //----------------------------------------------------------------------
-        public Button( Screen _screen, string _strText = "", Texture2D _iconTex = null, Anchor _anchor = Anchor.Center, string _strTooltipText="", object _tag=null )
-        : this( _screen, new ButtonStyle(
-                _screen.Style.ButtonCornerSize,
-                _screen.Style.ButtonFrame,
-                _screen.Style.ButtonDownFrame,
-                _screen.Style.ButtonHoverOverlay,
-                _screen.Style.ButtonDownOverlay,
-                _screen.Style.ButtonFocusOverlay,
-                _screen.Style.ButtonVerticalPadding,
-                _screen.Style.ButtonHorizontalPadding
-            ), _strText, _iconTex, _anchor, _strTooltipText, _tag )
+        public Button(Screen screen, string text = "", Texture2D iconTex = null, Anchor anchor = Anchor.Center, string tooltipText = "", object tag = null)
+        : this(screen, new ButtonStyle(
+                screen.Style.ButtonCornerSize,
+                screen.Style.ButtonFrame,
+                screen.Style.ButtonDownFrame,
+                screen.Style.ButtonHoverOverlay,
+                screen.Style.ButtonDownOverlay,
+                screen.Style.ButtonFocusOverlay,
+                screen.Style.ButtonVerticalPadding,
+                screen.Style.ButtonHorizontalPadding
+            ), text, iconTex, anchor, tooltipText, tag)
         {
         }
 
         //----------------------------------------------------------------------
         protected internal override void UpdateContentSize()
         {
-            ContentWidth    = ( (mIcon.Texture != null ) ? mIcon.ContentWidth : 0 ) + mLabel.ContentWidth + Padding.Horizontal + mMargin.Horizontal;
-            ContentHeight   = Math.Max( mIcon.ContentHeight, mLabel.ContentHeight ) + Padding.Vertical + mMargin.Vertical;
+            ContentWidth = ((mIcon.Texture != null) ? mIcon.ContentWidth : 0) + mLabel.ContentWidth + Padding.Horizontal + mMargin.Horizontal;
+            ContentHeight = Math.Max(mIcon.ContentHeight, mLabel.ContentHeight) + Padding.Vertical + mMargin.Vertical;
 
             base.UpdateContentSize();
         }
 
         //----------------------------------------------------------------------
-        public override void DoLayout( Rectangle _rect )
+        public override void DoLayout(Rectangle rectangle)
         {
-            base.DoLayout( _rect );
+            base.DoLayout(rectangle);
             HitBox = LayoutRect;
             Point pCenter = LayoutRect.Center;
 
-            switch( mAnchor )
+            switch (mAnchor)
             {
                 case UI.Anchor.Start:
-                    if( mIcon.Texture != null )
+                    if (mIcon.Texture != null)
                     {
-                        mIcon.DoLayout( new Rectangle( LayoutRect.X + Padding.Left + Margin.Left, pCenter.Y - mIcon.ContentHeight / 2, mIcon.ContentWidth, mIcon.ContentHeight ) );
+                        mIcon.DoLayout(new Rectangle(LayoutRect.X + Padding.Left + Margin.Left, pCenter.Y - mIcon.ContentHeight / 2, mIcon.ContentWidth, mIcon.ContentHeight));
                     }
 
                     mLabel.DoLayout(
                         new Rectangle(
-                            LayoutRect.X + Padding.Left + Margin.Left + ( mIcon.Texture != null ? mIcon.ContentWidth : 0 ), pCenter.Y - mLabel.ContentHeight / 2,
+                            LayoutRect.X + Padding.Left + Margin.Left + (mIcon.Texture != null ? mIcon.ContentWidth : 0), pCenter.Y - mLabel.ContentHeight / 2,
                             mLabel.ContentWidth, mLabel.ContentHeight
                         )
                     );
                     break;
-                case UI.Anchor.Center: {
-                    int iLabelWidth = mLabel.ContentWidth;
-                    int iLabelOffset = ContentWidth;
-
-                    if( mIcon.Texture != null )
+                case UI.Anchor.Center:
                     {
-                        mIcon.DoLayout( new Rectangle( pCenter.X - ContentWidth / 2 + Padding.Left + Margin.Left, pCenter.Y - mIcon.ContentHeight / 2, mIcon.ContentWidth, mIcon.ContentHeight ) );
-                    }
-                    else
-                    {
-                        iLabelWidth = Math.Min( mLabel.ContentWidth - mLabel.Padding.Horizontal, LayoutRect.Width ) + mLabel.Padding.Horizontal;
-                        iLabelOffset += iLabelWidth - mLabel.ContentWidth;
-                    }
+                        int iLabelWidth = mLabel.ContentWidth;
+                        int iLabelOffset = ContentWidth;
 
-                    mLabel.DoLayout(
+                        if (mIcon.Texture != null)
+                        {
+                            mIcon.DoLayout(new Rectangle(pCenter.X - ContentWidth / 2 + Padding.Left + Margin.Left, pCenter.Y - mIcon.ContentHeight / 2, mIcon.ContentWidth, mIcon.ContentHeight));
+                        }
+                        else
+                        {
+                            iLabelWidth = Math.Min(mLabel.ContentWidth - mLabel.Padding.Horizontal, LayoutRect.Width) + mLabel.Padding.Horizontal;
+                            iLabelOffset += iLabelWidth - mLabel.ContentWidth;
+                        }
 
-                        new Rectangle(
-                            pCenter.X - iLabelOffset / 2 + Padding.Left + Margin.Left + ( mIcon.Texture != null ? mIcon.ContentWidth : 0 ), pCenter.Y - mLabel.ContentHeight / 2,
-                            iLabelWidth, mLabel.ContentHeight
-                        )
-                    );
-                    break;
-                }
+                        mLabel.DoLayout(
+
+                            new Rectangle(
+                                pCenter.X - iLabelOffset / 2 + Padding.Left + Margin.Left + (mIcon.Texture != null ? mIcon.ContentWidth : 0), pCenter.Y - mLabel.ContentHeight / 2,
+                                iLabelWidth, mLabel.ContentHeight
+                            )
+                        );
+                        break;
+                    }
             }
         }
 
         //----------------------------------------------------------------------
-        public override void Update( float _fElapsedTime )
+        public override void Update(float elapsedTime)
         {
-            if( ! mPressedAnim.IsOver )
+            if (!mPressedAnim.IsOver)
             {
-                mPressedAnim.Update( _fElapsedTime );
+                mPressedAnim.Update(elapsedTime);
             }
 
             mTooltip.EnableDisplayTimer = mbIsHovered;
-            mTooltip.Update( _fElapsedTime );
+            mTooltip.Update(elapsedTime);
         }
 
         //----------------------------------------------------------------------
-        public override void OnMouseEnter( Point _hitPoint )
+        public override void OnMouseEnter(Point hitPoint)
         {
-            base.OnMouseEnter( _hitPoint );
+            base.OnMouseEnter(hitPoint);
             mbIsHovered = true;
         }
 
-        public override void OnMouseOut( Point _hitPoint )
+        public override void OnMouseOut(Point hitPoint)
         {
-            base.OnMouseOut( _hitPoint );
+            base.OnMouseOut(hitPoint);
             mbIsHovered = false;
             mTooltip.EnableDisplayTimer = false;
         }
 
         //----------------------------------------------------------------------
-        protected internal override bool OnMouseDown( Point _hitPoint, int _iButton )
+        protected internal override bool OnMouseDown(Point hitPoint, int button)
         {
-            if( _iButton != Screen.Game.InputMgr.PrimaryMouseButton ) return false;
+            if (button != Screen.Game.InputMgr.PrimaryMouseButton) return false;
 
-            Screen.Focus( this );
+            Screen.Focus(this);
             OnActivateDown();
 
             return true;
         }
 
-        protected internal override void OnMouseUp( Point _hitPoint, int _iButton )
+        protected internal override void OnMouseUp(Point hitPoint, int button)
         {
-            if( _iButton != Screen.Game.InputMgr.PrimaryMouseButton ) return;
+            if (button != Screen.Game.InputMgr.PrimaryMouseButton) return;
 
-            if( HitTest( _hitPoint ) == this )
+            if (HitTest(hitPoint) == this)
             {
                 OnActivateUp();
             }
@@ -308,15 +310,15 @@ namespace NuclearWinter.UI
         protected internal override bool OnActivateDown()
         {
             mbIsPressed = true;
-            mPressedAnim.SetTime( 0f );
+            mPressedAnim.SetTime(0f);
             return true;
         }
 
         protected internal override void OnActivateUp()
         {
-            mPressedAnim.SetTime( 0f );
+            mPressedAnim.SetTime(0f);
             mbIsPressed = false;
-            if( ClickHandler != null ) ClickHandler( this );
+            if (ClickHandler != null) ClickHandler(this);
         }
 
         protected internal override void OnBlur()
@@ -327,7 +329,7 @@ namespace NuclearWinter.UI
         //----------------------------------------------------------------------
         internal void ResetPressState()
         {
-            mPressedAnim.SetTime( 1f );
+            mPressedAnim.SetTime(1f);
             mbIsPressed = false;
         }
 
@@ -336,34 +338,34 @@ namespace NuclearWinter.UI
         {
             Texture2D frame = (!mbIsPressed) ? Style.Frame : Style.DownFrame;
 
-            if( frame != null )
+            if (frame != null)
             {
-                Screen.DrawBox( frame, LayoutRect, Style.CornerSize, Color.White );
+                Screen.DrawBox(frame, LayoutRect, Style.CornerSize, Color.White);
             }
 
-            Rectangle marginRect = new Rectangle( LayoutRect.X + Margin.Left, LayoutRect.Y + Margin.Top, LayoutRect.Width - Margin.Left - Margin.Right, LayoutRect.Height - Margin.Top - Margin.Bottom );
+            Rectangle marginRect = new Rectangle(LayoutRect.X + Margin.Left, LayoutRect.Y + Margin.Top, LayoutRect.Width - Margin.Left - Margin.Right, LayoutRect.Height - Margin.Top - Margin.Bottom);
 
-            if( mbIsHovered && ! mbIsPressed && mPressedAnim.IsOver )
+            if (mbIsHovered && !mbIsPressed && mPressedAnim.IsOver)
             {
-                if( Screen.IsActive && Style.HoverOverlay != null )
+                if (Screen.IsActive && Style.HoverOverlay != null)
                 {
-                    Screen.DrawBox( Style.HoverOverlay, marginRect, Style.CornerSize, Color.White );
+                    Screen.DrawBox(Style.HoverOverlay, marginRect, Style.CornerSize, Color.White);
                 }
             }
             else
-            if( mPressedAnim.CurrentValue > 0f )
+            if (mPressedAnim.CurrentValue > 0f)
             {
-                if( Style.DownOverlay != null )
+                if (Style.DownOverlay != null)
                 {
-                    Screen.DrawBox( Style.DownOverlay, marginRect, Style.CornerSize, Color.White * mPressedAnim.CurrentValue );
+                    Screen.DrawBox(Style.DownOverlay, marginRect, Style.CornerSize, Color.White * mPressedAnim.CurrentValue);
                 }
             }
 
-            if( Screen.IsActive && HasFocus && ! mbIsPressed )
+            if (Screen.IsActive && HasFocus && !mbIsPressed)
             {
-                if( Style.FocusOverlay != null )
+                if (Style.FocusOverlay != null)
                 {
-                    Screen.DrawBox( Style.FocusOverlay, marginRect, Style.CornerSize, Color.White );
+                    Screen.DrawBox(Style.FocusOverlay, marginRect, Style.CornerSize, Color.White);
                 }
             }
 
