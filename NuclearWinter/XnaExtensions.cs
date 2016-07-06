60c5e453da13a1835104c29dc37b2a944e72e24b
Robin Theilade <robintheilade@users.noreply.github.com>
2016-07-06 18:51:02 +0200

    Remove unused namespace usings Rename method parameters to get closer to Microsoft naming guidelines

--- NuclearWinter/XnaExtensions.cs
+++ NuclearWinter/XnaExtensions.cs
@@ -1,30 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
+using System;
 
 namespace NuclearWinter.Xna
 {
     public static class XnaExtensions
     {
-        public static Rectangle Clip( this Rectangle _rect1, Rectangle _rect2 )
+        public static Rectangle Clip(this Rectangle rectangle1, Rectangle rectangle2)
         {
             Rectangle rect = new Rectangle(
-                Math.Max( _rect1.X, _rect2.X ),
-                Math.Max( _rect1.Y, _rect2.Y ),
-                0, 0 );
+                Math.Max(rectangle1.X, rectangle2.X),
+                Math.Max(rectangle1.Y, rectangle2.Y),
+                0, 0);
 
-            rect.Width = Math.Min( _rect1.Right, _rect2.Right ) - rect.X;
-            rect.Height = Math.Min( _rect1.Bottom, _rect2.Bottom ) - rect.Y;
+            rect.Width = Math.Min(rectangle1.Right, rectangle2.Right) - rect.X;
+            rect.Height = Math.Min(rectangle1.Bottom, rectangle2.Bottom) - rect.Y;
 
-            if( rect.Width < 0 )
+            if (rect.Width < 0)
             {
                 rect.X += rect.Width;
                 rect.Width = 0;
             }
 
-            if( rect.Height < 0 )
+            if (rect.Height < 0)
             {
                 rect.Y += rect.Height;
                 rect.Height = 0;
@@ -33,9 +30,9 @@ namespace NuclearWinter.Xna
             return rect;
         }
 
-        public static Vector2 ToVector2( this Point _point )
+        public static Vector2 ToVector2(this Point _point)
         {
-            return new Vector2( _point.X, _point.Y );
+            return new Vector2(_point.X, _point.Y);
         }
     }
 }
