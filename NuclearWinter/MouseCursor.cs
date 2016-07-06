2abbca72af66ec5ce7db1c1ab63229f8b7f3383f
Robin Theilade <robintheilade@users.noreply.github.com>
2016-07-06 17:50:26 +0200

    Remove unused namespace usings

--- NuclearWinter/MouseCursor.cs
+++ NuclearWinter/MouseCursor.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace NuclearWinter
+﻿namespace NuclearWinter
 {
-        public enum MouseCursor {
+    public enum MouseCursor {
 #if !FNA
             Default,
             SizeWE,
