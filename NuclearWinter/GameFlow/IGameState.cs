90127efd6095e3c4e80d1f6bb4f338d12e818891
Robin Theilade <robintheilade@users.noreply.github.com>
2016-07-06 19:01:11 +0200

    Remove unused namespace usings Rename method parameters to get closer to Microsoft naming guidelines

--- NuclearWinter/GameFlow/IGameState.cs
+++ NuclearWinter/GameFlow/IGameState.cs
@@ -1,21 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace NuclearWinter.GameFlow
+﻿namespace NuclearWinter.GameFlow
 {
-    public interface IGameState<out T> where T:NuclearGame
+    public interface IGameState<out T> where T : NuclearGame
     {
         void Start();
         void Stop();
         void OnActivated();
         void OnExiting();
-        bool UpdateFadeIn( float _fElapsedTime );
+        bool UpdateFadeIn(float elapsedTime);
         void DrawFadeIn();
-        bool UpdateFadeOut( float _fElapsedTime );
+        bool UpdateFadeOut(float elapsedTime);
         void DrawFadeOut();
-        void Update( float _fElapsedTime );
+        void Update(float elapsedTime);
         void Draw();
 
     }
