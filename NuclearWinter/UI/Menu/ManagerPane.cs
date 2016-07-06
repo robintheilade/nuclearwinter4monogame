62321ebb5541292d0c2a1e2fe259d48ccee657bf
Robin Theilade <robintheilade@users.noreply.github.com>
2016-07-06 17:44:16 +0200

    Remove unused namespace usings Rename method parameters to get closer to Microsoft naming guidelines

--- NuclearWinter/UI/Menu/ManagerPane.cs
+++ NuclearWinter/UI/Menu/ManagerPane.cs
@@ -1,19 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace NuclearWinter.UI
+﻿namespace NuclearWinter.UI
 {
-    public abstract class ManagerPane<T>: Group where T:IMenuManager
+    public abstract class ManagerPane<T> : Group where T : IMenuManager
     {
-        public T                    Manager         { get; private set; }
+        public T Manager { get; private set; }
 
         //----------------------------------------------------------------------
-        public ManagerPane( T _manager )
-        : base( _manager.MenuScreen )
+        public ManagerPane(T manager)
+        : base(manager.MenuScreen)
         {
-            Manager     = _manager;
+            Manager = manager;
         }
     }
 }
