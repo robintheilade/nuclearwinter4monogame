562c0eeccb3ca57510e9510a0721b1b0b69aa9e0
Robin Theilade <robintheilade@users.noreply.github.com>
2016-07-06 19:07:09 +0200

    Remove unused namespace usings Rename method parameters to get closer to Microsoft naming guidelines

--- NuclearWinter/Animation/SmoothValue.cs
+++ NuclearWinter/Animation/SmoothValue.cs
@@ -1,47 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿
 using Microsoft.Xna.Framework;
 
 namespace NuclearWinter.Animation
 {
-    public class SmoothValue: AnimatedValue
+    public class SmoothValue : AnimatedValue
     {
         //----------------------------------------------------------------------
-        public SmoothValue( float _fStart, float _fEnd, float _fDuration, float _fDelay, AnimationLoop _loop )
+        public SmoothValue(float start, float end, float duration, float delay, AnimationLoop loop)
         {
-            Start       = _fStart;
-            End         = _fEnd;
-            Duration    = _fDuration;
-            Delay       = _fDelay;
-            Time        = 0f;
-            Loop        = _loop;
+            Start = start;
+            End = end;
+            Duration = duration;
+            Delay = delay;
+            Time = 0f;
+            Loop = loop;
         }
 
-        public SmoothValue( float _fStart, float _fEnd, float _fDuration, AnimationLoop _loop )
-        :   this ( _fStart, _fEnd, _fDuration, 0f, _loop )
-        {
-        }
-
-        public SmoothValue( float _fStart, float _fEnd, float _fDuration, float _fDelay )
-        :   this ( _fStart, _fEnd, _fDuration, _fDelay, AnimationLoop.NoLoop )
+        public SmoothValue(float start, float end, float duration, AnimationLoop loop)
+        : this(start, end, duration, 0f, loop)
         {
         }
 
-        public SmoothValue( float _fStart, float _fEnd, float _fDuration )
-        :   this ( _fStart, _fEnd, _fDuration, 0f, AnimationLoop.NoLoop )
+        public SmoothValue(float start, float end, float duration, float delay)
+        : this(start, end, duration, delay, AnimationLoop.NoLoop)
+        {
+        }
+
+        public SmoothValue(float start, float end, float duration)
+        : this(start, end, duration, 0f, AnimationLoop.NoLoop)
         {
         }
 
         //----------------------------------------------------------------------
         public override float CurrentValue
         {
-            get { return MathHelper.SmoothStep( Start, End, ( Time - Delay ) / Duration ); }
+            get { return MathHelper.SmoothStep(Start, End, (Time - Delay) / Duration); }
         }
 
         //----------------------------------------------------------------------
-        public float    Start;
-        public float    End;
+        public float Start;
+        public float End;
     }
 }
