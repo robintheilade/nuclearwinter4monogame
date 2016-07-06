8d0efc7a9d722f0ca1273e00fc8a39fd944169d0
Robin Theilade <robintheilade@users.noreply.github.com>
2016-07-06 17:22:56 +0200

    Remove unused namespace usings Rename method parameters to get closer to Microsoft naming guidelines

--- NuclearWinter/UI/Scrollbar.cs
+++ NuclearWinter/UI/Scrollbar.cs
@@ -1,127 +1,124 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
+using System;
 
 namespace NuclearWinter.UI
 {
-    public class Scrollbar: Widget
+    public class Scrollbar : Widget
     {
         //----------------------------------------------------------------------
-        public int              Offset;
-        int                     mMouseOffset;
+        public int Offset;
+        int mMouseOffset;
 
-        public int              Max
+        public int Max
         {
             get { return miMax; }
 
             set
             {
                 miMax = value;
-                Offset = Math.Min( Offset, miMax );
+                Offset = Math.Min(Offset, miMax);
             }
         }
 
-        public float            LerpOffset;
+        public float LerpOffset;
 
-        public bool             ScrollToBottom;
-
-        //----------------------------------------------------------------------
-        int                     miMax;
-
-        int                     miScrollbarHeight;
-        int                     miScrollbarOffset;
-
-        Point                   mMouseDragPoint;
-        Point                   mLastMouseDragPoint;
-
-        bool                    mbIsMouseDown;
-
-        Color                   mScrollbarColor;
-
-        public Rectangle        ScrollRect;
-
-        static Color            sDefaultColor = Color.White * 0.5f;
-        static Color            sHoveredColor = Color.White;
+        public bool ScrollToBottom;
 
         //----------------------------------------------------------------------
-        public Scrollbar( Screen _screen )
-        : base( _screen )
+        int miMax;
+
+        int miScrollbarHeight;
+        int miScrollbarOffset;
+
+        Point mMouseDragPoint;
+        Point mLastMouseDragPoint;
+
+        bool mbIsMouseDown;
+
+        Color mScrollbarColor;
+
+        public Rectangle ScrollRect;
+
+        static Color sDefaultColor = Color.White * 0.5f;
+        static Color sHoveredColor = Color.White;
+
+        //----------------------------------------------------------------------
+        public Scrollbar(Screen screen)
+        : base(screen)
         {
             mScrollbarColor = sDefaultColor;
         }
 
         //----------------------------------------------------------------------
-        public override void Update( float _fElapsedTime )
+        public override void Update(float elapsedTime)
         {
-            float fLerpAmount = Math.Min( 1f, _fElapsedTime * NuclearGame.LerpMultiplier );
-            LerpOffset = MathHelper.Lerp( LerpOffset, Offset, fLerpAmount );
-            LerpOffset = Math.Min( LerpOffset, Max );
+            float fLerpAmount = Math.Min(1f, elapsedTime * NuclearGame.LerpMultiplier);
+            LerpOffset = MathHelper.Lerp(LerpOffset, Offset, fLerpAmount);
+            LerpOffset = Math.Min(LerpOffset, Max);
         }
 
         //----------------------------------------------------------------------
-        public void DoLayout( Rectangle _rect, int _iContentHeight )
+        public void DoLayout(Rectangle rectangle, int contentHeight)
         {
-            ScrollRect = _rect;
+            ScrollRect = rectangle;
 
-            HitBox = new Rectangle( ScrollRect.Right - 10 - Screen.Style.VerticalScrollbar.Width / 2, ScrollRect.Y, Screen.Style.VerticalScrollbar.Width, ScrollRect.Height );
+            HitBox = new Rectangle(ScrollRect.Right - 10 - Screen.Style.VerticalScrollbar.Width / 2, ScrollRect.Y, Screen.Style.VerticalScrollbar.Width, ScrollRect.Height);
 
             bool bScrolledToBottom = ScrollToBottom && Offset >= Max;
 
-            Max = Math.Max( 0, _iContentHeight - ScrollRect.Height );
-            Offset = (int)MathHelper.Clamp( Offset, 0, Max );
+            Max = Math.Max(0, contentHeight - ScrollRect.Height);
+            Offset = (int)MathHelper.Clamp(Offset, 0, Max);
 
-            if( bScrolledToBottom )
+            if (bScrolledToBottom)
             {
                 Offset = Max;
             }
 
-            miScrollbarHeight = Math.Max( 10,  (int)( ( ScrollRect.Height - 20 ) / ( (float)_iContentHeight / ( ScrollRect.Height - 20 ) ) ) );
-            miScrollbarOffset = (int)( (float)LerpOffset / Max * (float)( ScrollRect.Height - 20 - miScrollbarHeight ) );
+            miScrollbarHeight = Math.Max(10, (int)((ScrollRect.Height - 20) / ((float)contentHeight / (ScrollRect.Height - 20))));
+            miScrollbarOffset = (int)((float)LerpOffset / Max * (float)(ScrollRect.Height - 20 - miScrollbarHeight));
         }
 
         //----------------------------------------------------------------------
-        public override void OnMouseEnter( Point _hitPoint )
+        public override void OnMouseEnter(Point hitPoint)
         {
             mScrollbarColor = sHoveredColor;
         }
 
-        public override void OnMouseOut( Point _hitPoint )
+        public override void OnMouseOut(Point hitPoint)
         {
             mScrollbarColor = sDefaultColor;
         }
 
-        protected internal override bool OnMouseDown( Point _hitPoint, int _iButton )
+        protected internal override bool OnMouseDown(Point hitPoint, int button)
         {
-            if( _iButton != Screen.Game.InputMgr.PrimaryMouseButton ) return false;
+            if (button != Screen.Game.InputMgr.PrimaryMouseButton) return false;
 
             mbIsMouseDown = true;
-            mLastMouseDragPoint = _hitPoint;
+            mLastMouseDragPoint = hitPoint;
 
             int iScrollBarBottom = ScrollRect.Top + miScrollbarOffset + Screen.Style.VerticalScrollbarCornerSize;
-            int iScrollBarTop    = ScrollRect.Top + miScrollbarOffset + miScrollbarHeight + Screen.Style.VerticalScrollbarCornerSize;
-            
-            if( ! ( mLastMouseDragPoint.Y > iScrollBarBottom && mLastMouseDragPoint.Y < iScrollBarTop ) )
-                Offset += ( mLastMouseDragPoint.Y - ( ScrollRect.Top + miScrollbarOffset + ( miScrollbarHeight / 2 )  ) ) * ScrollRect.Height / miScrollbarHeight;
+            int iScrollBarTop = ScrollRect.Top + miScrollbarOffset + miScrollbarHeight + Screen.Style.VerticalScrollbarCornerSize;
+
+            if (!(mLastMouseDragPoint.Y > iScrollBarBottom && mLastMouseDragPoint.Y < iScrollBarTop))
+                Offset += (mLastMouseDragPoint.Y - (ScrollRect.Top + miScrollbarOffset + (miScrollbarHeight / 2))) * ScrollRect.Height / miScrollbarHeight;
 
             mMouseOffset = Offset;
 
             return true;
         }
 
-        protected internal override void OnMouseUp( Point _hitPoint, int _iButton )
+        protected internal override void OnMouseUp(Point hitPoint, int button)
         {
-            if( _iButton != Screen.Game.InputMgr.PrimaryMouseButton ) return;
+            if (button != Screen.Game.InputMgr.PrimaryMouseButton) return;
 
             mbIsMouseDown = false;
         }
 
-        public override void OnMouseMove( Point _hitPoint )
+        public override void OnMouseMove(Point hitPoint)
         {
-            if( mbIsMouseDown )
+            if (mbIsMouseDown)
             {
-                mMouseDragPoint = _hitPoint;
+                mMouseDragPoint = hitPoint;
 
                 mMouseOffset += (mMouseDragPoint.Y - mLastMouseDragPoint.Y) * ScrollRect.Height / miScrollbarHeight;
                 Offset = mMouseOffset;
@@ -135,7 +132,7 @@ namespace NuclearWinter.UI
         //----------------------------------------------------------------------
         public override void Draw()
         {
-            if( miMax > 0 )
+            if (miMax > 0)
             {
                 Screen.DrawBox(
                     Screen.Style.VerticalScrollbar,
@@ -143,9 +140,9 @@ namespace NuclearWinter.UI
                         ScrollRect.Right - 5 - Screen.Style.VerticalScrollbar.Width / 2,
                         ScrollRect.Y + 10 + miScrollbarOffset,
                         Screen.Style.VerticalScrollbar.Width,
-                        miScrollbarHeight ),
+                        miScrollbarHeight),
                     Screen.Style.VerticalScrollbarCornerSize,
-                    mScrollbarColor );
+                    mScrollbarColor);
             }
         }
     }
