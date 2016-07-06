26e386a718e0ba3a750ee2d1ad67748f5f1ef7ce
Robin Theilade <robintheilade@users.noreply.github.com>
2016-07-06 17:49:56 +0200

    Remove unused namespace usings Rename method parameters to get closer to Microsoft naming guidelines

--- NuclearWinter/LocalizedKeyboardState.cs
+++ NuclearWinter/LocalizedKeyboardState.cs
@@ -1,47 +1,54 @@
 ﻿// Based on http://www.luminance.org/gruedorf/2009/08/14/supporting-alternate-keyboard-layouts-in-xna-games
 
-using System;
 using Microsoft.Xna.Framework.Input;
+using System;
 using System.Runtime.InteropServices;
 
 namespace NuclearWinter
 {
-    public struct LocalizedKeyboardState {
+    public struct LocalizedKeyboardState
+    {
 
-        internal enum MAPVK : uint {
+        internal enum MAPVK : uint
+        {
             VK_TO_VSC = 0,
             VSC_TO_VK = 1,
             VK_TO_CHAR = 2
         }
 
         [DllImport("user32.dll", CallingConvention = CallingConvention.Winapi, CharSet = CharSet.Auto, SetLastError = true)]
-        internal extern static uint MapVirtualKeyEx (uint key, MAPVK mappingType, IntPtr keyboardLayout);
+        internal extern static uint MapVirtualKeyEx(uint key, MAPVK mappingType, IntPtr keyboardLayout);
         [DllImport("user32.dll", CallingConvention = CallingConvention.Winapi, CharSet = CharSet.Auto, SetLastError = true)]
-        internal extern static IntPtr LoadKeyboardLayout (string keyboardLayoutID, uint flags);
+        internal extern static IntPtr LoadKeyboardLayout(string keyboardLayoutID, uint flags);
         [DllImport("user32.dll", CallingConvention = CallingConvention.Winapi, CharSet = CharSet.Auto, SetLastError = true)]
-        internal extern static bool UnloadKeyboardLayout (IntPtr handle);
+        internal extern static bool UnloadKeyboardLayout(IntPtr handle);
         [DllImport("user32.dll", CallingConvention = CallingConvention.Winapi, CharSet = CharSet.Auto, SetLastError = true)]
-        internal extern static IntPtr GetKeyboardLayout (IntPtr threadId);
+        internal extern static IntPtr GetKeyboardLayout(IntPtr threadId);
 
         internal const uint KLF_NOTELLSHELL = 0x00000080;
 
-        public struct KeyboardLayout : IDisposable {
+        public struct KeyboardLayout : IDisposable
+        {
             public readonly IntPtr Handle;
 
-            public KeyboardLayout (IntPtr handle) : this() {
+            public KeyboardLayout(IntPtr handle) : this()
+            {
                 Handle = handle;
             }
 
-            public KeyboardLayout (string keyboardLayoutID)
-                : this(LoadKeyboardLayout(keyboardLayoutID, KLF_NOTELLSHELL)) {
+            public KeyboardLayout(string keyboardLayoutID)
+                : this(LoadKeyboardLayout(keyboardLayoutID, KLF_NOTELLSHELL))
+            {
             }
 
-            public bool IsDisposed {
+            public bool IsDisposed
+            {
                 get;
                 private set;
             }
 
-            public void Dispose () {
+            public void Dispose()
+            {
                 if (IsDisposed)
                     return;
 
@@ -51,8 +58,10 @@ namespace NuclearWinter
 
             public static KeyboardLayout US_English = new KeyboardLayout("00000409");
 
-            public static KeyboardLayout Active {
-                get {
+            public static KeyboardLayout Active
+            {
+                get
+                {
                     return new KeyboardLayout(GetKeyboardLayout(IntPtr.Zero));
                 }
             }
@@ -64,18 +73,18 @@ namespace NuclearWinter
         static bool isWindows = SDL2.SDL.SDL_GetPlatform() == "Windows";
 #endif
 
-        public LocalizedKeyboardState( KeyboardState keyboardState )
+        public LocalizedKeyboardState(KeyboardState keyboardState)
         {
             Native = keyboardState;
         }
 
-        public bool IsKeyDown( Keys key, bool isLocalKey )
+        public bool IsKeyDown(Keys key, bool isLocalKey)
         {
             if (!isLocalKey) key = USEnglishToLocal(key);
             return Native.IsKeyDown(key);
         }
 
-        public bool IsKeyUp( Keys key, bool isLocalKey )
+        public bool IsKeyUp(Keys key, bool isLocalKey)
         {
             if (!isLocalKey)
                 key = USEnglishToLocal(key);
@@ -83,27 +92,27 @@ namespace NuclearWinter
             return Native.IsKeyUp(key);
         }
 
-        public bool IsKeyDown( Keys key )
+        public bool IsKeyDown(Keys key)
         {
-            return IsKeyDown( key, false );
+            return IsKeyDown(key, false);
         }
 
-        public bool IsKeyUp( Keys key )
+        public bool IsKeyUp(Keys key)
         {
-            return IsKeyUp( key, false );
+            return IsKeyUp(key, false);
         }
 
         // Maps a localized character like 'S' to the virtual scan code
         //  for that key on the user's keyboard ('O' in dvorak, for example)
-        public static Keys USEnglishToLocal( Keys _key )
+        public static Keys USEnglishToLocal(Keys key)
         {
-            if( _key > Keys.Z && ( _key < Keys.OemSemicolon || ( _key > Keys.OemTilde && ( _key < Keys.OemOpenBrackets || _key > Keys.OemBackslash ) ) ) ) return _key;
+            if (key > Keys.Z && (key < Keys.OemSemicolon || (key > Keys.OemTilde && (key < Keys.OemOpenBrackets || key > Keys.OemBackslash)))) return key;
 
 #if FNA
             if( isWindows )
 #endif
             {
-                return Windows_USEnglishToLocal(_key);
+                return Windows_USEnglishToLocal(key);
             }
 
 #if FNA
@@ -111,23 +120,23 @@ namespace NuclearWinter
 #endif
         }
 
-        public static Keys Windows_USEnglishToLocal( Keys _key )
+        public static Keys Windows_USEnglishToLocal(Keys key)
         {
-            var activeScanCode = MapVirtualKeyEx( (uint)_key, MAPVK.VK_TO_VSC, KeyboardLayout.US_English.Handle );
-            var nativeVirtualCode = MapVirtualKeyEx( activeScanCode, MAPVK.VSC_TO_VK, KeyboardLayout.Active.Handle );
+            var activeScanCode = MapVirtualKeyEx((uint)key, MAPVK.VK_TO_VSC, KeyboardLayout.US_English.Handle);
+            var nativeVirtualCode = MapVirtualKeyEx(activeScanCode, MAPVK.VSC_TO_VK, KeyboardLayout.Active.Handle);
 
             return (Keys)nativeVirtualCode;
         }
 
-        public static Keys LocalToUSEnglish( Keys _key )
+        public static Keys LocalToUSEnglish(Keys key)
         {
-            if( _key > Keys.Z ) return _key;
+            if (key > Keys.Z) return key;
 
 #if FNA
             if( isWindows )
 #endif
             {
-                return Windows_LocalToUSEnglish(_key);
+                return Windows_LocalToUSEnglish(key);
             }
 
 #if FNA
@@ -135,10 +144,10 @@ namespace NuclearWinter
 #endif
         }
 
-        public static Keys Windows_LocalToUSEnglish( Keys _key )
+        public static Keys Windows_LocalToUSEnglish(Keys key)
         {
-            var activeScanCode = MapVirtualKeyEx( (uint)_key, MAPVK.VK_TO_VSC, KeyboardLayout.Active.Handle );
-            var nativeVirtualCode = MapVirtualKeyEx( activeScanCode, MAPVK.VSC_TO_VK, KeyboardLayout.US_English.Handle );
+            var activeScanCode = MapVirtualKeyEx((uint)key, MAPVK.VK_TO_VSC, KeyboardLayout.Active.Handle);
+            var nativeVirtualCode = MapVirtualKeyEx(activeScanCode, MAPVK.VSC_TO_VK, KeyboardLayout.US_English.Handle);
 
             return (Keys)nativeVirtualCode;
         }
